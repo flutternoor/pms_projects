cafb52a99f83c780520b9233d76a0611bec63d49
noor <noor.ptcp@gmail.com>
2023-02-01 15:25:21 +0500

    works in BottomNavigationBar

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pms_projects/constants/constants.dart';
 import 'package:pms_projects/ui/home_screen/home_screen.dart';
 import 'package:pms_projects/ui/lectures_screen/lectures_screen.dart';
 import 'package:pms_projects/ui/library_screen/library_screen.dart';
@@ -12,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentIndex =0;
+ static int currentIndex =0;
   final  pages = [ const HomeScreen(), const TextScreen(), const LecturesScreen(), const LibraryScreen(), const ProfileScreen()];
 
   void onTapped(int index) {
@@ -27,48 +28,95 @@ class _HomePageState extends State<HomePage> {
       body:  pages[currentIndex],
 
       ///TODO: Here we change the background color of Bottom Navigation
-      bottomNavigationBar:  SizedBox(
-        height: 80,
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(
+      bottomNavigationBar:  Container(
+        decoration: const BoxDecoration(
+          borderRadius:  BorderRadius.only(
               topLeft: Radius.circular(40),
               topRight: Radius.circular(40)),
-          child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: const Color.fromARGB(255, 193, 209, 240),
-              iconSize: 20.0,
-              selectedIconTheme: const IconThemeData(size: 28.0),
-              selectedItemColor: const Color.fromARGB(255, 46, 90, 172),
-              unselectedItemColor: Colors.black,
-              selectedFontSize: 16.0,
-              unselectedFontSize: 12,
-              currentIndex: currentIndex,
-              onTap: onTapped,
-              items:  const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-
-                  icon: Icon(Icons.home),
-                  label: "Home",
-                ),
-                BottomNavigationBarItem(
-                  // backgroundColor: Colors.red,
-                  icon: Icon(Icons.search),
-                  label: "Test",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.settings),
-                  label: "Lectures",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.group),
-                  label: "Library",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: "profile",
-                ),
-              ]),
+          color: Colors.grey
         ),
+        height: 80,
+        child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white54,
+            iconSize: 24.0,
+            selectedIconTheme: const IconThemeData(size: 30.0,),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white54,
+            selectedFontSize: 13.0,
+            unselectedFontSize: 13,
+            selectedLabelStyle:  const TextStyle(fontWeight: FontWeight.bold,color: Colors.red,),
+            unselectedLabelStyle:
+            const TextStyle(color: Colors.grey, fontWeight: FontWeight.normal),
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            currentIndex: currentIndex,
+            onTap: onTapped,
+            items:  [
+              BottomNavigationBarItem(
+
+                icon:currentIndex== 0 ?Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: kAppColor
+                  ),
+                  child:  const Icon(Icons.home_outlined),
+                ):const Icon(Icons.home_outlined),
+                label: "Home",
+              ),
+               BottomNavigationBarItem(
+                // backgroundColor: Colors.red,
+                icon:currentIndex== 1 ?Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: kAppColor
+                  ),
+                  child:  const Icon(Icons.file_present),
+                ): Icon(Icons.file_present),
+                label: "Test",
+              ),
+               BottomNavigationBarItem(
+                icon: currentIndex== 2 ?Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: kAppColor
+                  ),
+                  child:  const Icon(Icons.featured_play_list_outlined),
+                )
+                    :Icon(Icons.featured_play_list_outlined),
+                label: "Lectures",
+              ),
+               BottomNavigationBarItem(
+                icon: currentIndex== 3 ?Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: kAppColor
+                  ),
+                  child:  const Icon(Icons.library_books_outlined),
+                ): Icon(Icons.library_books_outlined),
+                label: "Library",
+              ),
+               BottomNavigationBarItem(
+                icon: currentIndex== 4 ?Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: kAppColor
+                  ),
+                  child:  const Icon(Icons.person_outline),
+                ): Icon(Icons.person_outline),
+                label: "profile",
+              ),
+            ]),
       ),
 
 
